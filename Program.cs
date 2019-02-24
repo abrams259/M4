005b0e4e97ea112cef02efc9fe36f2ba7c306990
Sherman259 <47825335+Sherman259@users.noreply.github.com>
2019-02-24 19:58:10 +0100

    Convert

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+          int num = Convert.ToInt32 ("45");
+            Console.WriteLine("num+7");
+            Console.ReadLine();
+            
+        }
+    }
+
+    
+    {
+    }
+}
